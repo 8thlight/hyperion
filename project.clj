c154a76f3608c0189b5aef603e5dba33ec769174
Myles Megyesi <mylesmegyesi@me.com>
2012-03-02 16:22:29 -0600

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject hyperion "0.0.1-SNAPSHOT"
+  :description "A library for storing data."
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[speclj "2.1.1"]])
---
+++ spec/hyperion/core_spec.clj
@@ -0,0 +1,6 @@
+(ns hyperion.core-spec
+  (:use [speclj.core]
+        [hyperion.core]))
+
+(describe "hyperion core"
+  (it "is alive"))
