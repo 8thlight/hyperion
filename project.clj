0d604715ae20c45817e73125db278bf4c2e7c668
Micah <micah@8thlight.com>
2012-03-06 15:49:58 -0600

    core.jar ... yuk... renamed to hyperion-core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hyperion/core "1.0.0"
+(defproject hyperion/hyperion-core "1.0.0"
   :description "A library for storing data."
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
   :dev-dependencies [[speclj "2.1.1"]]
