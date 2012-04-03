bdc5a7f144f131f8aadf1add6574ca76f43e779e
Myles Megyesi <mylesmegyesi@me.com>
2012-04-02 20:11:36 -0500

    moves postgres into a separate lein project

--- postgres/project.clj
+++ postgres/project.clj
@@ -1,6 +1,6 @@
-(defproject hyperion/hyperion-core "1.0.1"
+(defproject hyperion/hyperion-postgres "1.0.0"
   :description "A library for storing data."
-  :dependencies [[org.clojure/clojure "1.4.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta6"]
                  [hyperion/hyperion-core "1.0.1"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [org.clojure/java.jdbc "0.1.1"]]
