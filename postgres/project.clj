35696142b4d9679f42e0ab4b002bee92bc25f57d
Myles Megyesi <mylesmegyesi@me.com>
2012-04-06 15:07:05 -0500

    update version in pg

--- postgres/project.clj
+++ postgres/project.clj
@@ -1,7 +1,7 @@
 (defproject hyperion/hyperion-postgres "1.0.0"
   :description "A library for storing data."
   :dependencies [[org.clojure/clojure "1.4.0-beta6"]
-                 [hyperion/hyperion-core "1.0.1"]
+                 [hyperion/hyperion-core "1.0.2"]
                  [hyperion/hyperion-sql "1.0.0"]
                  [postgresql/postgresql "8.4-702.jdbc4"]]
   :dev-dependencies [[speclj "2.1.1"]]
