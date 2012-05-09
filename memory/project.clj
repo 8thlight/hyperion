81967559e098412743e64caab841983ae46da9ed
Micah <micah@8thlight.com>
2012-05-09 15:26:35 -0500

    removed memory subproject

--- memory/project.clj
+++
@@ -1,8 +0,0 @@
-(def config (load-file "../config.clj"))
-
-(defproject hyperion/hyperion-memory (:version config)
-  :description "A library for storing data."
-  :dependencies [[org.clojure/clojure ~(:clojure-version config)]
-                 [hyperion/hyperion-core ~(:version config)]]
-  :dev-dependencies [[speclj ~(:speclj-version config)]]
-  :test-path "spec")
